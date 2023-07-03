9e784d5cf69ef84cd30943a2b80515968ec1d954
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-07-03 12:54:15 -0300

    versão com requisitos e validações ok, sem limit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
 import { MongoClient } from "mongodb";
+import Joi from "joi";
 
 
 
@@ -20,34 +21,49 @@ mongoClient.connect()
  .then(() => db = mongoClient.db())
  .catch((err) => console.log(err.message))
 
-const users = [];
-const messages =[];
 
 
-app.post("/participants", (req, res) =>{
+
+app.post("/participants", async(req, res) =>{
     const {name} = req.body;
-    if(!req.body.name){
-        res.sendStatus(422);
-    }else{
-        const newUser = {
-            name,
-            lastStatus : Date.now()
-        };
-        const login = { 
-            from: name,
-            to: 'Todos',
-            text: 'entra na sala...',
-            type: 'status',
-            time: dayjs().format('HH:mm:ss')
-    };
+    const schemaUser = Joi.object({
+        name: Joi.string().required()
+    });
 
-        const promisse = 
-            db.collection("participants").insertOne(newUser);
-            db.collection("messages").insertOne(login);
-        
-        promisse.then(() => res.sendStatus(201))
-  
+    const validation = schemaUser.validate(req.body, {abortEarly: false});
+    if(validation.error){
+        const errors = validation.error.details.map(msgErr => msgErr.message);
+        return res.status(422).send(errors);
     }
+
+    const newUser = {
+        name,
+        lastStatus : Date.now()
+    };
+    const login = { 
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs().format('HH:mm:ss')
+};
+
+    try{
+
+        const userCreated = await db.collection("participants").findOne(req.body);
+        if(userCreated) return res.status(409).send("Nome já cadastrado");
+
+        await db.collection("participants").insertOne(newUser);
+        await db.collection("messages").insertOne(login);
+        res.sendStatus(201);
+
+
+    }catch (err){
+        res.send(err).sendStatus(500);
+    }
+
+    
+    
 });
 
 app.get("/participants", (req, res) => {
@@ -57,14 +73,44 @@ app.get("/participants", (req, res) => {
     promisse.catch(error => res.status(500).send(error.message));
 });
 
-app.post("/messages", (req, res) =>{
+app.post("/messages", async(req, res) =>{
     const {to, text, type} = req.body;
+    
     const user = req.headers.user;
     const newMessage = {from: user, to, text, type, time: dayjs().format('HH:mm:ss')};
+    console.log(req.headers)
+    const schemaMsg = Joi.object({
+            to: Joi.string().required().min(1),
+            text: Joi.string().required().min(1),
+            type: Joi.string().required().valid("message","private_message")
+    });
 
-    const promisse = 
-    db.collection("messages").insertOne(newMessage);
-    promisse.then(() => res.sendStatus(201))
+    const validation = schemaMsg.validate(req.body, {abortEarly: false});
+    if(validation.error){
+        const errors = validation.error.details.map(msgErr => msgErr.message);
+        return res.status(422).send(errors);
+    };
+
+    const schemaFrom = Joi.object({from: Joi.required()});
+    
+    const validation2 = schemaFrom.validate(req.headers.from,{abortEarly: false});
+    if(validation2.error){
+        const errors = validation2.error.details.map(msgErr=>msgErr.message);
+        return res.status(422).send(errors);
+    };
+
+
+
+    try{
+        const useroff = await db.collection("participants").findOne({name: user});
+        if(!useroff) return res.status(422).send("usuário offline");
+
+        await db.collection("messages").insertOne(newMessage);
+        res.sendStatus(201)
+    }catch(err){
+        res.send(err).sendStatus(500);
+    }
+    
 
 });
 
